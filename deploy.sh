c49cf1f67bc039a5dea102b323931bd334231413
Daren McCulley <mcculleydj@gmail.com>
2020-12-26 23:25:43 -0500

    deploy

--- deploy.sh
+++ deploy.sh
@@ -5,9 +5,9 @@ cd util
 python3 generate_json.py
 cd ..
 
-# echo "Building and deploying the Vue application..."
-# cd frontend
-# yarn build
-# rsync --delete -azv -e "ssh -i ~/.ssh/mcculleydj.dev.pem" dist/ ubuntu@54.203.119.188:/home/ubuntu/rosetta-code
-# ssh -i ~/.ssh/mcculleydj.dev.pem ubuntu@54.203.119.188 sudo systemctl restart nginx
-# cd ..
+echo "Building and deploying the Vue application..."
+cd frontend
+yarn build
+rsync --delete -azv -e "ssh -i ~/.ssh/mcculleydj.dev.pem" dist/ ubuntu@54.203.119.188:/home/ubuntu/rosetta-code
+ssh -i ~/.ssh/mcculleydj.dev.pem ubuntu@54.203.119.188 sudo systemctl restart nginx
+cd ..
